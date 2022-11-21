7ec01ac210753821c74c45358af5ebf9df7c16fd
HugoAndreL <hugo.andre.lucena@outlook.com>
2022-11-21 17:25:39 -0300

    memoteca-alura-angular

---
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos'
+
+  constructor(private http: HttpClient) { }
+
+  // Funcionalide de ler
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
+
+  // Funcionalidade de criar
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento);
+  }
+
+  // Funcionalidade de editar
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`
+    return this.http.put<Pensamento>(url, pensamento)
+  }
+
+  // Funcionalidade de excluir
+  excluir(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
+
+  buscar(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
+}
